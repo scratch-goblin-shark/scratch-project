0657d77c144d13d5397bd2f4826c5e4cbc9c6855
Jung Ho Lee <jungholee@Jungs-MacBook-Pro.local>
2021-05-07 17:21:44 -0400

    more boilerplate commits

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function MainContainer() {
+
+  
+}
+
+
+export default MainContainer;
--- server/server.js
+++ server/server.js
@@ -2,11 +2,33 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('../client/assets'));
+app.use(express.json());
 
 
 
 
 
+
+
+
+
+app.get('*', (req, res) => {
+  res.status(404).send('Nothing here');
+})
+
+app.use((err, req, res, next) => {
+  const defaultError = {
+    status: 500,
+    log: 'Problem in some middleware',
+    message: 'Serverside problem',
+  }
+  const ourError = Object.assign(defaultError, err);
+
+  console.log(ourError.log);
+
+  res.status(ourError.status).send(ourError.message);
+})
 
 
 app.listen('http://localhost:3000')
