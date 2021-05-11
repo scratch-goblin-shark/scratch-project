281308ef1f8572e46437386cdfd0df71e27512cf
Erik <erikmat@berkeley.edu>
2021-05-11 14:57:18 -0700

    twillio api and cron job added

--- server/server.js
+++ server/server.js
@@ -1,33 +1,50 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const keys = require('./api_keys');
+const twilio = require('twilio')(keys.twilioAccountSid, keys.twilioAuthToken);
 
 const app = express();
 
-const userController = require("./controllers/userController");
+const userController = require('./controllers/userController');
 
-app.use(express.static("../client/assets"));
+app.use(express.static('../client/assets'));
 app.use(express.json());
+
+// once we have DB figured out we can query the DB every minute.
+setInterval(() => {
+  console.log('set interval is working per minute');
+}, 60 * 1000);
+
+twilio.messages
+  .create({
+    body: 'NAME reported they were feeling down. Reach out!',
+    messagingServiceSid: 'MG7fb60d87d0007c008da8c8476ed45d95',
+    to: '+19088388678',
+  })
+  .then((message) => console.log(message.sid))
+  .done();
 
 // app.get("/", (request, response) => {
 //   response.status(200).sendFile(path.join(__dirname, "../index.html"));
 // });
-const uriArr = ["/login", "/signup", "user", "/"];
+const uriArr = ['/login', '/signup', 'user', '/'];
 uriArr.map((e) =>
-app.get(e, (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "../index.html"));
-}));
+  app.get(e, (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  })
+);
 
-app.use("/build", express.static(path.join(__dirname, "../build")));
+app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.post(
-  "/login",
+  '/login',
   userController.verifyUser,
   userController.getMoodHistory,
   userController.updateLastLoginDate,
   (request, response) => {
     const responseObject = {
       userVerified: true,
-      message: "User Found.",
+      message: 'User Found.',
       firstName: response.locals.user[0].firstname,
       addiction: response.locals.user[0].addiction,
       emergencyContactName: response.locals.user[0].emergencycontactname,
@@ -39,14 +56,14 @@ app.post(
   }
 );
 
-app.post("/signup", userController.createUser, (request, response) => {
+app.post('/signup', userController.createUser, (request, response) => {
   return response
     .status(200)
-    .json({ newUserCreated: true, message: "New user successfully created." });
+    .json({ newUserCreated: true, message: 'New user successfully created.' });
 });
 
 app.post(
-  "/user",
+  '/user',
   userController.getUserID,
   userController.saveMood,
   userController.getMoodHistory,
@@ -57,15 +74,15 @@ app.post(
   }
 );
 
-app.get("*", (request, response) => {
-  response.status(404).send("Nothing here");
+app.get('*', (request, response) => {
+  response.status(404).send('Nothing here');
 });
 
 app.use((error, request, response, next) => {
   const defaultError = {
     status: 500,
-    log: "Problem in some middleware.",
-    message: "Serverside problem.",
+    log: 'Problem in some middleware.',
+    message: 'Serverside problem.',
   };
   const ourError = Object.assign(defaultError, error);
 
