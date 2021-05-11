f2b8029c6cb7930094511aa8fd075120f53da05d
Graham Albachten <albachteng@gmail.com>
2021-05-11 17:39:51 -0500

    minor changes to connect to new sql server

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,10 +1,9 @@
 const { Pool } = require('pg');
-
-const PG_URI = 'postgres://ewobuldh:v_gE8Fwm8URxSuIa7mdgposKZ6br9Q6N@queenie.db.elephantsql.com:5432/ewobuldh';
+require('dotenv').config();
 
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString: PG_URI
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = {
