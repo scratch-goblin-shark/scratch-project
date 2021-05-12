76eb1664460675c40be0a1bdbc8775957a3a25bc
erik r <erikrogel3@gmail.com>
2021-05-12 18:40:06 -0500

    removed exposed api keys

--- server/api/send_sms.js
+++ server/api/send_sms.js
@@ -1,12 +1,16 @@
-var accountSid = 'ACf49202c0379ab189685a846e0ab20295'; // Your Account SID from www.twilio.com/console
-var authToken = '7cc6af342eb3c5d16f8712f7cd998e70';   // Your Auth Token from www.twilio.com/console
+// const apiKeys = require('../../.env')
+require('dotenv').config;
 
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'testing testing testing!',
-    to: '+17189625815',  // Text this number
+    body: 'HEY! HI HELLOOO! - Erik',
+    to: '+19732558470',  // Text this number
     from: '+18578582454' // From a valid Twilio number
 })
 .then((message) => console.log(message.sid));
